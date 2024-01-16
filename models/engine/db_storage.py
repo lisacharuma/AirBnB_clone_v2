f4ca830ad2039c3c7e8657573c5c83280b204ce9
DeFatso <115782937+DeFatso@users.noreply.github.com>
2024-01-16 15:54:17 +0000

    models/__init__.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -25,3 +25,47 @@ class DBStorage:
                 bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
+
+    def all(self, cls=None):
+        """Queries all objects of a certain class"""
+        from models.base_model import BaseModel, Base
+        classes = [BaseModel, User, State, City, Amenity, Place, Review]
+
+        if cls:
+            classes = [cls] if type(cls) == type else [cls.__class__]
+
+        objects = {}
+        for class_ in classes:
+            for obj in self.__session.query(class_).all():
+                key = "{}.{}".format(obj.__class__.__name__, obj.id)
+                objects[key] = obj
+
+        return objects
+
+    def new(self, obj):
+        """Adds a new object to the current database session."""
+        self.__session.add(obj)
+
+    def save(self):
+        """Commits all changes of the current database session."""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """Deletes obj from the current database session."""
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """Creates all tables in the database and reloads session."""
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(
+                bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
