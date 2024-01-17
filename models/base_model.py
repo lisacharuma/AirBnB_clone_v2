78565daeb6060d3d2b5f05f40c097c76396e7670
DeFatso <115782937+DeFatso@users.noreply.github.com>
2024-01-17 15:29:26 +0000

    fixed the circular import

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ import uuid
 from datetime import datetime
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-from models import storage
+#from models import storage
 
 Base = declarative_base()
 
@@ -17,6 +17,7 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
+        from models import storage
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
@@ -43,6 +44,7 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when the instance is changed"""
+        from models import storage
         self.updated_at = datetime.utcnow()
         storage.save()
 
---
+++ models/engine/db_storage.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""DBStorage module"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from os import getenv
+from models.base_model import Base
+
+class DBStorage:
+    """DBStorage class"""
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """Initialization method"""
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}:3306/{}'
+                                      .format(getenv('HBNB_MYSQL_USER'),
+                                              getenv('HBNB_MYSQL_PWD'),
+                                              getenv('HBNB_MYSQL_HOST'),
+                                              getenv('HBNB_MYSQL_DB')),
+                                      pool_pre_ping=True)
+        if getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """Query on the current database session"""
+        from models import storage
+        classes = ["User", "State", "City", "Amenity", "Place", "Review"]
+        objects = {}
+        if cls:
+            query_result = self.__session.query(eval(cls)).all()
+            for obj in query_result:
+                key = "{}.{}".format(cls, obj.id)
+                objects[key] = obj
+        else:
+            for class_name in classes:
+                query_result = self.__session.query(eval(class_name)).all()
+                for obj in query_result:
+                    key = "{}.{}".format(class_name, obj.id)
+                    objects[key] = obj
+        return objects
+
+    def new(self, obj):
+        """Add the object to the current database session"""
+        self.__session.add(obj)
+
+    def save(self):
+        """Commit all changes of the current database session"""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """Delete from the current database session"""
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """Create all tables in the database and create the current database session"""
+        Base.metadata.create_all(self.__engine)
+        Session = scoped_session(sessionmaker(bind=self.__engine,
+                                              expire_on_commit=False))
+        self.__session = Session()
