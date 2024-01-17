6f9d76351a5f5c8a8f621a6f73ec04305a3bf16b
DeFatso <115782937+DeFatso@users.noreply.github.com>
2024-01-17 14:34:40 +0000

    reverted all the changes from task 6

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
-"""This module initializes the storage variable to FileStorage or DBStorage"""
+"""This module instantiates an object of class FileStorage"""
+from models.engine.file_storage import FileStorage
 
-from os import getenv
 
-HBNB_TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
-
-if HBNB_TYPE_STORAGE == 'db':
-    from models.engine.db_storage import DBStorage
-    storage = DBStorage()
-else:
-    from models.engine.file_storage import FileStorage
-    storage = FileStorage()
-
+storage = FileStorage()
 storage.reload()
--- models/state.py
+++ models/state.py
@@ -1,22 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
 
-class State(BaseModel, Base):
+
+class State(BaseModel):
     """ State class """
-    __tablename__ = 'states'
-
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete-orphan")
-
-    @property
-    def cities(self):
-        """ Getter attribute for cities in FileStorage """
-        from models import storage
-        city_list = []
-        for city in storage.all("City").values():
-            if city.state_id == self.id:
-                city_list.append(city)
-        return city_list
+    name = ""
