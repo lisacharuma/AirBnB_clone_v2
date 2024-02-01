4872ccce620dab17fe4f516e05d0be88d065d086
DeFatso <115782937+DeFatso@users.noreply.github.com>
2024-02-01 03:15:38 +0000

    pycodestyle

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -16,8 +16,10 @@ def do_pack():
         local("mkdir -p versions")
 
         time_format = "%Y%m%d%H%M%S"
-        archive_name = "web_static_{}.tgz".format(datetime.utcnow().strftime(time_format))
+        archive_name = "web_static_{}.tgz".format(
+                datetime.utcnow().strftime(time_format)
+        )
         local("tar -cvzf versions/{} web_static".format(archive_name))
         return os.path.join("versions", archive_name)
-    except:
+    except Exception as e:
         return None
