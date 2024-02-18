7e236a9f0660e2553bcb3ca19a46ecbb20d32c8d
DeFatso <115782937+DeFatso@users.noreply.github.com>
2024-02-18 01:54:27 +0000

    models/state.py

--- models/state.py
+++ models/state.py
@@ -4,19 +4,24 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
+
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
 
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete-orphan")
 
-    @property
-    def cities(self):
-        """ Getter attribute for cities in FileStorage """
-        from models import storage
-        city_list = []
-        for city in storage.all("City").values():
-            if city.state_id == self.id:
-                city_list.append(city)
-        return city_list
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """ Getter attribute for cities in FileStorage """
+            from models import storage
+            city_list = []
+            for city in storage.all("City").values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
+
+    else:  # DBStorage
+        cities = relationship("City", backref="state",
+                              cascade="all, delete-orphan")
