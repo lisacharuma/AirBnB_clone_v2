95e0e9b614491a9d3134c76a6f0f13db1546a42d
lisacharuma <charumalisah@gmail.com>
2024-02-21 00:20:10 -0800

    Deploys achive to server

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,6 +10,7 @@ env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
 def do_deploy(archive_path):
+    """ Deploys achive to server"""
      try:
          if not (path.exists(archive_path)):
              return False
