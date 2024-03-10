8296eabc89d80669b9499f81caccf9253cbe583f
lisacharuma <charumalisah@gmail.com>
2024-03-10 15:49:25 -0700

    Number templates

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -35,9 +35,10 @@ def number_route(n):
     return "{} is a number".format(n)
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
+@app.route('/number_template/<n>', strict_slashes=False)
 def number_template(n):
-    return render_template('5-number_template.html', n=n)
+    if isinstance(n, int):
+        return render_template('5-number_template.html', n=n)
 
 
 if __name__ == '__main__':
